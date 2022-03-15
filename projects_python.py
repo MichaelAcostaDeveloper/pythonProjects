10481e25f31bc436e346ee7e28a4c0c58fb64ff2
MichaelAcostaDeveloper <acostamichael80desarrollo@gmail.com>
2022-03-15 15:00:59 -0500

    add changes in the module of math

--- projects_python.py
+++ projects_python.py
@@ -1,23 +1,47 @@
+from asyncio import sleep
 import math
+from re import T
+############FUNCIONES MATEMATICAS#############
+#funcion de seleccion de elementos
+def seleciona(a,b):
+    pregunta=int(input('''Que valor ingresado desea que sea el argumento de la funcion seleccionada:
+                    Digite 1 para el valor ''' + str(a) + '''
+                    Digite 2 para el valor ''' + str(b) + '''
+                    -> '''))
+    if pregunta==1:
+        return 1
+    elif pregunta==2:
+        return 2
+    else:
+        return seleciona(a,b)
+
+#funcion de confirmacion de valores
+def confirma(a,b):
+    return print("-----------------------> Los numeros seleccionados son: "+str(a) +" y "+str(b))
+
 #funcion suma
 def suma(a,b):
     s=a+b
-    return print("El resultado: "+str(s)+".")
+    return print("-----------------------> El resulado: "+str(s)+".")
+
 #funcion resta
 def resta(a,b):
     r=a-b
-    return print("El resultado: "+str(r)+".")
+    return print("-----------------------> El resultado: "+str(r)+".")
+
 #funcion producto
 def producto(a,b):
     p=a*b
-    return print("El resultado: "+str(p)+".")
+    return print("-----------------------> El resultado: "+str(p)+".")
+
 #funcion radicacion
 def alertasAlgebraicasRadicaion(a):
     while a<0:
         print("El valor ingresado no debe ser negativo")
         a=float(input("Ingrese el valor nuevamente: "))
     raiz=pow(a,0.5)
-    return print("El resultado: "+str(raiz)+".")
+    return print("-----------------------> El resultado: "+str(raiz)+".")
+
 #funcion division
 def alertasAlgebraicasDivision(a,b):
     while b==0:
@@ -25,6 +49,7 @@ def alertasAlgebraicasDivision(a,b):
         b=float(input("Ingrese el segundo valor nuevamente: "))
     division=a/b
     return print("El cociente es: "+str(division)+".")
+
 #funcion logaritmos
 def alertasAlgebraicasLogaritmos(base,argumento):
     while (float(base)<=0 or float(base)==1):
@@ -35,6 +60,7 @@ def alertasAlgebraicasLogaritmos(base,argumento):
         argumento=float(input("Ingrese el valor nuevamente: "))
     respuesta=math.log(argumento,base)
     return print("El resultado: "+str(respuesta)+".")
+
 #funcion exponencial
 def alertasAlgebraicasExponentes(base,exponente):
     while (float(base)<=0 or float(base)==1):
@@ -42,10 +68,12 @@ def alertasAlgebraicasExponentes(base,exponente):
         base=float(input("Ingrese el valor nuevamente: "))
     respuesta=pow(base,exponente)
     return print("El resultado: "+str(respuesta)+".")
+
 #funcion potenciacion
 def ejecucionPotencia(a,b):
     respuesta=pow(a,b)
     return print("La potencia es: "+str(respuesta)+".")
+
 #funcion trigonometrica
 def ejecucionTrigonometria(a):
     definicion=int(input('''Seleccione la funcion trigonometrica a calcular:
@@ -59,19 +87,26 @@ def ejecucionTrigonometria(a):
     elif definicion==2:
         resultado=math.cos(float(a))
         return print("El coseno del valor ingresado es: "+str(resultado) + '.')
-    else:
+    elif definicion==3:
         if a==0:
             return print("La tangente de este valor esta indefinida.")
         else:
             resultado=math.tan(float(a))
         return print("La tangente del valor ingresado es: "+str(resultado) + '.')
+    else:
+        ejecucionTrigonometria(a)
 
+#funcion de transformacion de radianes a grados
+def tranf_rad_degree(a,b):
+    a_1=math.degrees(a)
+    b_1=math.degrees(b)
+    return print('Los angulos pasados a grados son: '+str(a)+'rad = '+str(round(a_1,2))+'° y ' + str(b)+'rad = '+str(round(b_1,2))+'°')
 
-###################################################################################################################################################################################
+######################################################################################################################################################################################
 #menu del modulo de operaciones basicas
 def operaciones_basicas(val1,val2):
     while True:
-        operacion=int(input("""Seleccione la operacion que desea:
+        operacion=int(input("""***Seleccione la operacion que desea:***
         1. suma
         2. resta
         3. producto
@@ -84,60 +119,58 @@ def operaciones_basicas(val1,val2):
         0. salir de este modulo
         -> """))
         if operacion==1:
+            print('SUMA')
+            confirma(val1,val2)
             suma(val1,val2)
         elif operacion==2:
+            print('RESTA')
             resta(val1,val2)
+            confirma(val1,val2)
         elif operacion==3:
+            print('PRODUCTO')
+            confirma(val1,val2)
             producto(val1,val2)
         elif operacion==4:
+            print('DIVISION')
+            confirma(val1,val2)
             alertasAlgebraicasDivision(val1,val2)
         elif operacion==5:
-            pregunta=int(input('''Que valor ingresado desea que sea el radicando:
-                                Digite 1 para el valor ''' + str(val1) + '''
-                                Digite 2 para el valor ''' + str(val2) + '''
-                                -> '''))
-            if pregunta==1:
+            print('RAIZ CUADRADA')
+            confirma(val1,val2)
+            if seleciona(val1,val2)==1:
                 alertasAlgebraicasRadicaion(val1)
-            elif pregunta==2:
-                alertasAlgebraicasRadicaion(val2)
             else:
-                print('Ingrese una valor valido!!!')
+                alertasAlgebraicasRadicaion(val2)
         elif operacion==6:
-            pregunta=int(input('''Que valor ingresado desea que sea la base:
-                                Digite 1 para el valor ''' + str(val1) + '''
-                                Digite 2 para el valor ''' + str(val2) + '''
-                                -> '''))
-            if pregunta==1:
+            print('POTENCIACION')
+            confirma(val1,val2)
+            if seleciona(val1,val2)==1:
                 ejecucionPotencia(val1,val2)
             else:
                 ejecucionPotencia(val2,val1)
         elif operacion==7:
-            pregunta=int(input('''Que valor ingresado desea que sea la base:
-                                Digite 1 para el valor ''' + str(val1) + '''
-                                Digite 2 para el valor ''' + str(val2) + '''
-                                -> '''))
-            if pregunta==1:
+            print('LOGARITMOS')
+            confirma(val1,val2)
+            if seleciona(val1,val2)==1:
                 alertasAlgebraicasLogaritmos(val1,val2)
             else:
                 alertasAlgebraicasLogaritmos(val2,val1)
         elif operacion==8:
-            pregunta=int(input('''Que valor ingresado desea que sea la base:
-                                Digite 1 para el valor ''' + str(val1) + '''
-                                Digite 2 para el valor ''' + str(val2) + '''
-                                -> '''))
-            if pregunta==1:
+            print('EXPONENTES')
+            confirma(val1,val2)
+            if seleciona(val1,val2)==1:
                 alertasAlgebraicasExponentes(val1,val2)
             else:
                 alertasAlgebraicasExponentes(val2,val1)
         elif operacion==9:
-            pregunta=int(input('''Que valor ingresado desea que sea el argumento de la funcion trigonometrica:
-                                Digite 1 para el valor ''' + str(val1) + '''
-                                Digite 2 para el valor ''' + str(val2) + '''
-                                -> '''))
-            if pregunta==1:
+            print('USO DE LA TRIGONOMETRIA')
+            confirma(val1,val2)
+            if seleciona(val1,val2)==1:
                 ejecucionTrigonometria(val1)
+                tranf_rad_degree(val1,val2)
             else:
                 ejecucionTrigonometria(val2)
+                tranf_rad_degree(val1,val2)
         elif operacion==0:
             run()
         else:
@@ -146,7 +179,7 @@ def operaciones_basicas(val1,val2):
 #MENU PRINCIPAL
 def run():
     while True:
-        opcion=int(input("""BIENVENIDO AL PROYECTO
+        opcion=int(input("""****BIENVENIDO AL PROYECTO****
         Selecciona una opcion:
         1. Operaciones básicas.
         2. Contando letras.
@@ -154,14 +187,15 @@ def run():
         4. Salir
         -> """))
         if opcion==1:
-            print('...Bienvenido al modulo de operaciones basicas...')
+            print('...BIENVENIDO AL MODULO DE OPERACIONES BASICAS...')
             val1=float(input("Ingrese el primer valor: "))
             val2=float(input("Ingrese el segundo valor: "))
             operaciones_basicas(val1,val2)
         elif opcion==2:
-            print('...Bienvenido al modulo Contando letras...')
+            print('...BIENVENIDO AL MODULO DE CONTEO DE LETRAS...')
+            word1=input('Ingrese una palabra o frase para su analisis:')
         elif opcion==3:
-            print('...Bienvenido al modulo Aplicacion con datos...')
+            print('...BIENVENIDO AL MODULO DE APLICACION CON DATOS...')
         elif opcion==4:
             print('Un placer automatizar sus procesos!!!')
             exit()
